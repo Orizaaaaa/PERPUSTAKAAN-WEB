49ae28940c6367ebeddfc62a100ae32cd625d46e
oryzaaa <oryzasativacikal@gmail.com>
2023-06-01 09:00:17 +0700

    Upgrade Struktur

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,4 +3,20 @@ $('.caraosel').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+
 });
