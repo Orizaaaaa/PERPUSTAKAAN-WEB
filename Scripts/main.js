fd9b97c33d146846a889ff4ad99e0017f1806e98
oryzaaa <oryzasativacikal@gmail.com>
2023-06-02 17:03:36 +0700

    menambahkan popup sekaligus judul buku

--- Scripts/main.js
+++ Scripts/main.js
@@ -20,3 +20,22 @@ $('.caraosel').slick({
     ]
 
 });
+
+
+// Temukan semua tombol dengan class "btn" yang memiliki atribut "data-bs-toggle" dengan nilai "modal"
+let buttons = document.querySelectorAll('button[data-bs-toggle="modal"]');
+
+buttons.forEach(function (button) {
+    // Tambahkan event listener untuk menghandle klik pada setiap tombol
+    button.addEventListener('click', function () {
+        // Dapatkan judul dari tombol yang diklik
+        let bookTitle = this.parentNode.querySelector('h6').innerText;
+
+        // Temukan elemen modal title
+        let modalTitle = document.querySelector('.modal-title');
+
+        // Perbarui judul modal dengan judul buku yang dipilih
+        modalTitle.innerText = bookTitle;
+    });
+});
+
